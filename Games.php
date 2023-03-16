bc3397cf2b6a2827cc4a4850380b2bd99d2c658a
EmilianoOnTheRunn <116228144+EmilianoOnTheRunn@users.noreply.github.com>
2023-03-16 11:16:43 -0400

    Add files via upload

---
+++ Games.php
@@ -0,0 +1,113 @@
+<?php
+// Game 1
+$letters = array(); 
+
+for ($i = 0; $i <= 6; $i++) {
+  $letters[$i] = chr(rand(26)); 
+}
+
+echo "Our letters: " . implode(", ", $letters) . "\n";
+echo "Please arrange them in ascending order: ";
+
+
+$user_input;
+
+
+if ($user_input == sort($letters)) {
+  echo "Correct!";
+} else {
+  echo "Incorrect.";
+}
+?>
+
+<?php
+// Game 2
+$letters = array(); 
+
+for ($i = 0; $i <= 6; $i++) {
+  $letters[$i] = chr(rand(26)); 
+}
+
+echo "Our letters: " . implode(", ", $letters) . "\n";
+echo "Please arrange them in ascending order: ";
+
+
+$user_input;
+
+
+if ($user_input == rsort($letters)) {
+  echo "Correct!";
+} else {
+  echo "Incorrect.";
+}
+?>
+
+<?php
+// Game 3
+$numbers = array(); 
+
+for ($i = 0; $i <= 6; $i++) {
+  $numbers[$i] = rand(26); 
+}
+
+echo "Our numbers: " . implode(", ", $numbers) . "\n";
+echo "Please arrange them in ascending order: ";
+
+
+$user_input;
+
+
+if ($user_input == sort($numbers)) {
+  echo "Correct!";
+} else {
+  echo "Incorrect.";
+}
+?>
+
+
+<?php
+// Game 4
+$numbers = array(); 
+
+for ($i = 0; $i <= 6; $i++) {
+  $numbers[$i] = rand(26); 
+}
+
+echo "Our numbers: " . implode(", ", $numbers) . "\n";
+echo "Please arrange them in ascending order: ";
+
+
+$user_input;
+
+
+if ($user_input == rsort($numbers)) {
+  echo "Correct!";
+} else {
+  echo "Incorrect.";
+}
+?>
+
+
+<?php
+// Game 5
+$letters = array(); 
+
+for ($i = 0; $i <= 6; $i++) {
+  $letters[$i] = chr(rand(26)); 
+}
+
+echo "Our letters: " . implode(", ", $letters) . "\n";
+echo "Please arrange them in ascending order: ";
+
+
+$user_input;
+
+
+if ($user_input == sort($letters)) {
+  echo "Correct!";
+} else {
+  echo "Incorrect.";
+}
+?>
+
+
